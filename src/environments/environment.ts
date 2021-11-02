1d0824cb64db0895bd13a1f32c7d2f36c8c79b0f
Deleke <76471637+Deleke@users.noreply.github.com>
2021-11-02 20:07:51 +0100

    Restructure 4

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+export const firebase_configuration = {
+  apiKey: "AIzaSyCI4-K7wpTRDBdT12PadtoDcnlrLCzIjGM",
+  authDomain: "ignitemegastars-5376e.firebaseapp.com",
+  projectId: "ignitemegastars-5376e",
+  storageBucket: "ignitemegastars-5376e.appspot.com",
+  messagingSenderId: "559858827116",
+  appId: "1:559858827116:web:009fb00c14ab0fa35bfd49",
+  measurementId: "G-G2XMWWFYPQ"
+}
