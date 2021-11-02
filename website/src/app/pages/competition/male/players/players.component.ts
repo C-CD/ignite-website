730d3960cf85b6ab9696b57c7961d53686f2c70b
Friday Davis <55281305+sirdavis99@users.noreply.github.com>
2021-11-02 12:15:41 +0100

    update on player page

--- website/src/app/pages/competition/male/players/players.component.ts
+++ website/src/app/pages/competition/male/players/players.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
+  players: any[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.players = [1,2,3,4,5,6,7,8,9];
   }
+
+
+
+
 
 }
